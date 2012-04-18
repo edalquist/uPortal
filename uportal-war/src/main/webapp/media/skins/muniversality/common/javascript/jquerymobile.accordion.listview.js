d0f891b8f479652312230d4be892df4fcfc32a8f
Jacob Lichner <jacob.d.lichner@gmail.com>
2012-04-18 16:25:04 -0700

    All tabs now initially collapsed #43

--- uportal-war/src/main/webapp/media/skins/muniversality/common/javascript/jquerymobile.accordion.listview.js
+++ uportal-war/src/main/webapp/media/skins/muniversality/common/javascript/jquerymobile.accordion.listview.js
@@ -17,26 +17,19 @@
  * under the License.
  */
 (function($) {
-	$.fn.jqmAccordion = function(options) {
-		var settings = $.extend({
-			'categoryShown': 0
-		}, options);	
+ 	$.fn.jqmAccordion = function(options) {
+ 		var settings = $.extend({
+ 			'categoryShown': 'none'
+ 		}, options);	
 
-		function init() {
-			$('li[data-role=list-divider]')
-				.eq(settings.categoryShown)
-				.nextUntil('li[data-role=list-divider]')
-				.css('display', 'block');
-		}
-		init();
-		return this.delegate('.ui-li-divider', 'click', function() {
-			var $this = $(this);
-			if($this.next().is(':visible')) {
-				return;
-			}
-			$('.ui-listview > .ui-li:not(.ui-li-divider)').slideUp(150);
-			$this.nextUntil('.ui-li-divider').slideDown(300);
-		});
-	};
+ 		return this.delegate('.ui-li-divider', 'click', function() {
+ 			var $this = $(this);
+ 			if($this.next().is(':visible')) {
+ 				return;
+ 			}
+ 			$('.ui-listview > .ui-li:not(.ui-li-divider)').slideUp(150);
+ 			$this.nextUntil('.ui-li-divider').slideDown(300);
+ 		});
+ 	};
 })( jQuery );
 
